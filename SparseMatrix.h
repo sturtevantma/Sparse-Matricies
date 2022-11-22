b87b5b977e264a0bc7c8a68611dee816ff9e2c2c
sturtevantma <sturtevantma21@gmail.com>
2022-11-22 17:30:29 -0500

    Combined SparseMatrix.h and SparseNode.h

--- SparseMatrix.h
+++ SparseMatrix.h
@@ -1,5 +1,11 @@
-#include "SparseNode.h"
 #include <list>
+
+struct SparseNode {
+  int x; // The horizontal posistion of the node ( 0 is left column )
+  int y; // The vertical posistion of the node ( 0 is top row )
+  int val; // The value at (x,y)
+}
+
 class SparseMatrix {
   private:
     std::list<SparseNode> matrix; // The linked list of SparseNodes, see https://github.com/sturtevantma/Sparse-Matricies for info
--- SparseNode.h
+++
@@ -1,5 +0,0 @@
-struct SparseNode {
-    int x; // The horizontal posistion of the node ( 0 is left column )
-    int y; // The vertical posistion of the node ( 0 is top row )
-    int val; // The value at (x,y)
-}
