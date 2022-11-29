a52437fa9dc6e40faeffa55edfcda15a5f541fd2
Matthew Sturtevant <65269630+sturtevantma@users.noreply.github.com>
2022-11-29 16:05:17 -0500

    Changed SparseMatrix and SparseNode
    Re organized SparseMatrix so it has a linked list structure

--- SparseMatrix.h
+++ SparseMatrix.h
@@ -4,15 +4,17 @@ struct SparseNode {
   int x; // The horizontal posistion of the node ( 0 is left column )
   int y; // The vertical posistion of the node ( 0 is top row )
   int val; // The value at (x,y)
+  SparseNode* next_; // A pointer to the next node
 }
 
 class SparseMatrix {
   private:
-    std::list<SparseNode> matrix; // The linked list of SparseNodes, see https://github.com/sturtevantma/Sparse-Matricies for info
     int M; // The number of rows
     int N; // The number of columns
 
   public:
+    SparseNode* head; // A pointer to the head node
+  
     SparseMatrix(); // Constructor for SparseMatrix
     void append_node(int x, int y, int val); // Adds a node into the appropriate position
     void remove_node(int x, int y); // Removes a node based on coords (x,y)
