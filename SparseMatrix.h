7cfa955b72a24640aeeca884124cc5fe91488f00
fernans728 <98556788+fernans728@users.noreply.github.com>
2022-11-29 17:46:17 -0500

    Update SparseMatrix.h

--- SparseMatrix.h
+++ SparseMatrix.h
@@ -1,22 +1,28 @@
+//
+// Created by Nathan Fernandes on 11/29/22.
+//
+
+#ifndef FINAL_PROJECT_SPARCEMATRIX_H
+#define FINAL_PROJECT_SPARCEMATRIX_H
 #include <list>
 
 struct SparseNode {
-  int x; // The horizontal posistion of the node ( 0 is left column )
-  int y; // The vertical posistion of the node ( 0 is top row )
-  int val; // The value at (x,y)
-  SparseNode* next_; // A pointer to the next node
+    int x; // The horizontal posistion of the node ( 0 is left column )
+    int y; // The vertical posistion of the node ( 0 is top row )
+    int val; // The value at (x,y)
+    SparseNode* next_; // A pointer to the next node
 };
 
 class SparseMatrix {
-  private:
+private:
     int M; // The number of rows
     int N; // The number of columns
 
-  public:
+public:
     SparseNode* head; // A pointer to the head node
-  
+
     SparseMatrix(int M, int N); // Constructor for SparseMatrix
-    void append_node(int x, int y, int val); // Adds a node into the appropriate position
+    void append_node(int x, int y, int val, SparseNode *n); // Adds a node into the appropriate position
     void remove_node(int x, int y); // Removes a node based on coords (x,y)
 
     SparseMatrix operator+(const SparseMatrix& matrix2); // Defines behavior for the addition operator
@@ -28,3 +34,6 @@ class SparseMatrix {
     SparseMatrix right_multiply(SparseMatrix matrix2); // Function for right multiplication
     SparseMatrix add(SparseMatrix matrix2); // Function for addition
 };
+
+
+#endif //FINAL_PROJECT_SPARCEMATRIX_H
